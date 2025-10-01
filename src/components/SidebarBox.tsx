a1bbc45bbbd8a73e171182cff75f9b56b7565a37
Nils Hartmann <nils@nilshartmann.net>
2025-10-01 12:57:13 +0200

    React Context für globalen State (Dummy-Code)

--- src/components/SidebarBox.tsx
+++ src/components/SidebarBox.tsx
@@ -1,9 +1,8 @@
 import { ReactNode } from "react";
 
-import { H2 } from "@/components/Heading";
 
 type SidebarBoxProps = {
-  title: string;
+  title: ReactNode;
   children: ReactNode;
 };
 
@@ -11,7 +10,7 @@ export function SidebarBox({ title, children }: SidebarBoxProps) {
   return (
     <div className={"border-1 w-full rounded-2xl bg-slate-50 px-4 py-8"}>
       <div className={"mb-8 flex justify-center"}>
-        <H2>{title}</H2>
+        {title}
       </div>
       {children}
     </div>
